6b9f29bd5bb2fa0c53f3312e5e587c05d435c536
Vadris <72710015+Vadris@users.noreply.github.com>
2022-06-14 22:20:03 +0200

    Added Stuff

--- src/main/java/org/v4dris/math/FXConverter.java
+++ src/main/java/org/v4dris/math/FXConverter.java
@@ -2,14 +2,19 @@ package org.v4dris.math;
 
 import javafx.scene.Group;
 import javafx.scene.shape.Line;
+import org.v4dris.math.point.*;
 import org.v4dris.math.vector.Vector3D;
 
 public class FXConverter {
-    public Line vectorToLine(Vector3D vector){
+    public static Line vectorToLine(Vector3D vector){
         return null;
     }
 
-    public Group bodyToGroup(){
+    public static Group bodyToGroup(){
+        return null;
+    }
+
+    public static Point2D convertPoint(Point3D){
         return null;
     }
 }
